071900a22038b7541fc78ab2bef24f886ed4fe65
r4z4c <usia.mueller@googlemail.com>
2020-11-14 10:20:47 +0100

    print send message in joinGame

--- main.py
+++ main.py
@@ -202,8 +202,8 @@ def joingame(update, context, gameName):
 	if update.message.from_user.id in userID:
 		context.bot.send_message(chat_id=update.message.chat_id, text="Du bist dem Spiel berreits beigetreten.")
 	else:
-		context.bot.send_message(chat_id=update.message.chat_id, text="Du bist drin!")
-		cur = db.tquery("INSERT INTO game_user (g_name, c_id, m_id, user_text) VALUES (%s, %s, %s, %s)", (gameName, update.message.from_user.id, update.message.message_id, ""))
+		print(context.bot.send_message(chat_id=update.message.chat_id, text="Du bist drin!"))
+		cur = db.tquery("INSERT INTO game_user (g_name, c_id, m_id, user_text) VALUES (%s, %s, %s, %s)", (gameName, update.message.from_user.id, update.message.message_id+1, ""))
 		db.commit()
 		updateMessage(context, gameName)
 
