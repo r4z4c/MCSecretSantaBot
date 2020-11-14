81e5dd9b8865fc74b54442641e877b6a4e9d5f48
r4z4c <usia.mueller@googlemail.com>
2020-11-14 15:25:32 +0100

    debug

--- main.py
+++ main.py
@@ -229,6 +229,7 @@ def joingame(update, context, gameName):
 		updateMessage(context, gameName)
 		cur = db.tquery("SELECT text FROM game WHERE name = %s", (gameName,))
 		userHasText = db.commit()
+		print(userHasText)
 		if userHasText == 1:
 			gtext.append([update.message.from_user.id, update.message.message_id+1])
 			context.bot.send_message(chat_id=update.message.chat_id, text="Das Spiel hat die Spielernachricht aktiviert, bitte gib eine Nachricht ein.")
