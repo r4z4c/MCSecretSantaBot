a4089b7702d02f1710323fa599ff36422a65aac0
r4z4c <usia.mueller@googlemail.com>
2020-11-14 11:17:12 +0100

    minor fix

--- main.py
+++ main.py
@@ -132,13 +132,14 @@ def checkUser(update, context):
 	theUser = []
 	for user in allUsers:
 		if user[0] == update.message.from_user.id:
+			print(update.message.from_user.id)
 			theUser = user
 
 	if len(theUser) == 0:
 		cur = db.tquery("INSERT INTO user (u_id, first_name, last_name, username) VALUES (%s, %s, %s, %s)", (update.message.from_user.id, ("" if update.message.from_user.first_name == None else update.message.from_user.first_name), ("" if update.message.from_user.last_name == None else update.message.from_user.last_name), update.message.from_user.username))
 		db.commit()
 	else:
-		cur = db.tquery("UPDATE  user SET first_name=%s, last_name=%s, username=%s WHERE u_id=%s", (update.message.from_user.first_name, update.message.from_user.last_name, update.message.from_user.username, update.message.from_user.id))
+		cur = db.tquery("UPDATE user SET first_name=%s, last_name=%s, username=%s WHERE u_id=%s", (update.message.from_user.first_name, update.message.from_user.last_name, update.message.from_user.username, update.message.from_user.id))
 		db.commit()
 	return True
 
