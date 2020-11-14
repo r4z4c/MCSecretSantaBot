9612e68578490ca48adf8b3482af99ace466d0bf
r4z4c <usia.mueller@googlemail.com>
2020-11-14 10:04:45 +0100

    message not found

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ def joingame(update, context, gameName):
 	else:
 		cur = db.tquery("INSERT INTO game_user (g_name, c_id, m_id, user_text) VALUES (%s, %s, %s, %s)", (gameName, update.message.from_user.id, update.message.message_id+1, ""))
 		db.commit()
-		print(context.bot.send_message(chat_id=update.message.chat_id, text="."))
+		context.bot.send_message(chat_id=update.message.chat_id, text="Du bist drin!")
 		updateMessage(context, gameName)
 
 def creategame(update, context):
