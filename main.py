3015f7dac713f44d4c35c10ab7d2cf427b00f12d
r4z4c <usia.mueller@googlemail.com>
2020-11-14 15:35:29 +0100

    debug

--- main.py
+++ main.py
@@ -311,7 +311,7 @@ def buttonHandler(update, context):
 				db.commit()
 				updateMessage(context, gameName)
 				cur = db.tquery("SELECT text FROM game WHERE name = %s", (gameName,))
-				userHasText = cur.fetchall()
+				userHasText = cur.fetchall()[0][0]
 				print(userHasText)
 				if userHasText == 1:
 					gtext.append([update.message.from_user.id, update.message.message_id+1])
