260a5831a11dd769e88135eff340384355f13ab7
r4z4c <usia.mueller@googlemail.com>
2020-11-14 10:29:08 +0100

    fixed message to edit not found

--- main.py
+++ main.py
@@ -117,12 +117,10 @@ def updateMessage(context, gameName):
 
 	for i in tmpUser:
 		userID.append(i[3])
-		messageID.append(4)
+		messageID.append(i[4])
 
 	for i in range(len(userID)):
 		if userID[i] != guID:
-			print(userID[i])
-			print(messageID[i])
 			context.bot.edit_message_text(text=message, chat_id=int(userID[i]), message_id=int(messageID[i]), reply_markup=user_markup)
 	print(guID)
 	print(gmID)
